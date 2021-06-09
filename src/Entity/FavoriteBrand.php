c305b0c696dafcca39f46bd269469de60db16118
Florent <florent.gallitre@gmail.com>
2021-06-09 09:45:36 +0200

    :card_file_box: update favorite brand

--- src/Entity/FavoriteBrand.php
+++ src/Entity/FavoriteBrand.php
@@ -23,6 +23,23 @@ class FavoriteBrand
      */
     private $idUser;
 
+    /**
+     * @ORM\OneToOne(targetEntity=brand::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $firstFav;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Brand::class, cascade={"persist", "remove"})
+     */
+    private $secondFav;
+
+    /**
+     * @ORM\OneToOne(targetEntity=brand::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $thirdFav;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -36,6 +53,42 @@ class FavoriteBrand
     public function setIdUser(user $idUser): self
     {
         $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    public function getFirstFav(): ?brand
+    {
+        return $this->firstFav;
+    }
+
+    public function setFirstFav(brand $firstFav): self
+    {
+        $this->firstFav = $firstFav;
+
+        return $this;
+    }
+
+    public function getSecondFav(): ?Brand
+    {
+        return $this->secondFav;
+    }
+
+    public function setSecondFav(?Brand $secondFav): self
+    {
+        $this->secondFav = $secondFav;
+
+        return $this;
+    }
+
+    public function getThirdFav(): ?brand
+    {
+        return $this->thirdFav;
+    }
+
+    public function setThirdFav(brand $thirdFav): self
+    {
+        $this->thirdFav = $thirdFav;
 
         return $this;
     }
